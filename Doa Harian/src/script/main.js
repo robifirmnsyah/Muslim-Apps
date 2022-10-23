ffbc0fb63d7d4c2cc7840043e0be562ce575636c
Robi Firmansyah <86773628+robifirmnsyah@users.noreply.github.com>
2022-10-24 06:12:28 +0700

    Update main.js
    menambahkan fitur search

--- Doa Harian/src/script/main.js
+++ Doa Harian/src/script/main.js
@@ -1,26 +1,53 @@
+const main = document.getElementById('main');
+const search = document.getElementById('search');
+const form = document.getElementById('form');
+
 function getDoa() {
+  fetch('https://tanyadoa-api.herokuapp.com/all')
+    .then(response => response.json())
+    .then(data => {
+      if (data.results !== 0) {
+        showDoa(data.results);
+      } else {
+        main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+      }
+      console.log(data.results);
+    })
+}
+
+
+function showDoa() {
   fetch('https://tanyadoa-api.herokuapp.com/all')
     .then(response => response.json())
     .then(response => {
       let cardDoa = '';
-      Object.values(response).forEach(doa => {
+      Object.values(response.data).forEach(doa => {
         cardDoa += `
-        <div class="col-lg-3 col-md-4 col-sm-12">
-        <div class="card mb-4 card-doa">
-          <div class="card-body">
-            <h5 class="card-title">${doa.nama}</h5>
-            <h3 class="card-subtitle mb-2 text-muted text-end">${doa.lafal}</h3>
-            <p class="card-text">${doa.arti}</p>
+          <div class="col-lg-10 col-md-10 col-sm-10 ">
+          <div class="card mb-4 card-doa-list">
+            <div class="card-body">
+              <h5 class="card-title text-center mb-4">${doa.nama}</h5>
+              <h3 class="card-subtitle mb-3 text-center">${doa.lafal}</h3>
+              <p class="card-text text-center">"${doa.arti}"</p>
+            </div>
           </div>
         </div>
-      </div>
-        `;
+          `;
       });
       const listDoa = document.querySelector('.card-doa');
       listDoa.innerHTML = cardDoa;
 
-      console.log(response)
     });
+
 }
 
-getDoa();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm) {
+    getDoa('https://tanyadoa-api.herokuapp.com/search/' + searchTerm)
+  } else {
+    getDoa(URL);
+  }
+  console.log(searchTerm);
+})
